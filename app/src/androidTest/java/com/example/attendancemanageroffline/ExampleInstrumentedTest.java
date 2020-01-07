8f6c5420277258c9e9e29cc7f3449f5e0eaf4029
Arjun <arjun.prs@gmail.com>
2020-01-07 18:11:41 +0530

    v1.0.4

--- app/src/androidTest/java/com/example/attendancemanageroffline/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/attendancemanageroffline/ExampleInstrumentedTest.java
@@ -3,11 +3,17 @@ package com.example.attendancemanageroffline;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -23,5 +29,8 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.attendancemanageroffline", appContext.getPackageName());
+        ActivityTestRule<MainActivity> activityRule;
+        activityRule = new ActivityTestRule<>(MainActivity.class);
+        onView(withText("SMART ATTENDANCE MANAGER")).check(matches(isDisplayed()));
     }
 }
