d6288aa7fcf21f9a942d5116a84d1c549e75e35e
Arjun R <arjun.prs@gmail.com>
2020-03-14 18:24:00 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/attendancemanageroffline/MainActivity.java
+++ app/src/main/java/com/example/attendancemanageroffline/MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-// the change
+
 import static android.content.Context.MODE_PRIVATE;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
