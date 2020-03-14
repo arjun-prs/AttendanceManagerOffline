26e936cd37bb52c8b6d7a706f1338f94f18ed841
Arjun R <arjun.prs@gmail.com>
2020-03-14 20:39:48 +0530

    Update adminInterface.java

--- app/src/main/java/com/example/attendancemanageroffline/adminInterface.java
+++ app/src/main/java/com/example/attendancemanageroffline/adminInterface.java
@@ -19,7 +19,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-//check for statci code analysis
+//check for static code analysis 2
 public class adminInterface extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
     SQLiteDatabase db;
     Button submit;
